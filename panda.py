6d024a5ce96c1d113d1ab5a9af7c73fb1aaebabe
dillinger, austin <atdillinger92@gmail.com>
2018-03-12 21:29:39 -0500

    panda.py: figuring out dataframes

--- panda.py
+++ panda.py
@@ -3,28 +3,18 @@ import pandas as pd
 # Execution python -c 'import panda; panda.convertsql(1)'
 
 def convertsql(dataType):
-    
     wb = (pd.read_clipboard())
-    print(type(wb))
-    print(wb.iloc[0])
-    print(type(wb.iloc[0]))
 
-    data = []
-    print(data)
-    data.insert(0,2131321)
-    print(data)
+    header = list(wb)
 
-    pd.concat([pd.DataFrame(data),wb], ignore_index=True)
+    #REMOVE value from list
+    print(header)
 
+    #APPEND value to dataframe
+    wb.append(header)
 
-    print(wb)
     
-    #print(list(wb.index)[0])
-    #print(wb.parse(1))
-
-    #print(wb.pd.DataFrame(index=2))
-    #print(type(wb))
-
+    print(wb.values)
 
     
 
