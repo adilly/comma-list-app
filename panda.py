b440d093f8f4b92f449b7fb9bf98c65afdb4ea7e
dillinger, austin <atdillinger92@gmail.com>
2018-03-30 21:31:40 -0500

    pandas.py: getting data out of data as individual values

--- panda.py
+++ panda.py
@@ -1,13 +1,29 @@
 import pandas as pd
 
-# Execution python -c 'import panda; panda.convertsql(1)'
+# Execution python -c 'import panda; panda.convertsql()'
+"""
+100010
+20
+452
+1245
+45254
+24245
+"""
 
-def convertsql(dataType):
-    wb = (pd.read_clipboard(header=None))
-
-    for ind, values in wb.iteritems():
-        print(values)
-
+def convertsql():
+    wb = (pd.read_clipboard(header=None).values)
+    for x in wb:
+        print(isinstance(x,str))
+        print(type(x))
+        print(x,"test")
+    """ elif isinstance(wb,int):
+        for x in wb:
+            print("test",x,"test")
+        return
+    else:
+        print('This is not a valid SQL type.')
+        return None """
+        
     #print(wb.values)
 
     
